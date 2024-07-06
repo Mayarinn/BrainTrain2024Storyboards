2229be08536333ea778439df7c6bd0be75fad8e0
Karina Osadchaya <dyach.zero@gmail.com>
2024-07-06 12:19:36 +0300

    ADD: Set AnswerTextfield placeholder text to answer value

--- ViewController.swift
+++ ViewController.swift
@@ -21,6 +21,7 @@ class ViewController: UIViewController {
         TitleLabel.text = ""
         TaskLabel.textAlignment = .right
         TaskLabel.text = createTask()
+        AnswerTextfield.placeholder = String(Answer)
     }
     
     func createTask() -> String {
