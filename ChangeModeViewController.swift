aa5e061e41ed34815a5711abf42de0a0f003dd45
Karina Osadchaya <dyach.zero@gmail.com>
2024-07-12 12:33:04 +0300

    ADD: Set MultiplyLabel text to Multiplication

--- ChangeModeViewController.swift
+++ ChangeModeViewController.swift
@@ -14,6 +14,7 @@ class ChangeModeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         TitleLabel.text = "Change Mode"
+        MultiplyLabel.text = "Multiplication"
     }
     
 }
