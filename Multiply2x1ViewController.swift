5bb6cc5399de125a8b6fb45d7ef1d5689c01a73d
Karina Osadchaya <dyach.zero@gmail.com>
2024-07-18 12:13:59 +0300

    FIX: My name in file

--- Multiply2x1ViewController.swift
+++ Multiply2x1ViewController.swift
@@ -2,7 +2,7 @@
 //  Multiply2x1ViewController.swift
 //  BrainTrain2024Storyboards
 //
-//  Created by Maÿarinn Tindómë on 7/18/24.
+//  Created by Karina Osadchaya on 7/18/24.
 //
 
 import Foundation
